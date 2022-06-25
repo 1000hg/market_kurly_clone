158b21ee983d313a2e818156fa9948383047635c
yskim <alxzkf123@gmail.com>
2022-06-26 05:45:04 +0900

    build

--- packages/server/server.js
+++ packages/server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 app.get('/', (req, res) => {
     res.sendFile('index.html')
