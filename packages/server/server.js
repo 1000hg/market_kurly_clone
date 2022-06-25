032c8dad7bb4ebde4272e05d044e03a6d3ee82a2
yskim <alxzkf123@gmail.com>
2022-06-26 06:14:09 +0900

    build

--- packages/server/server.js
+++ packages/server/server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile('../client/build/index.html')
+    res.send('한글 되니?');
 })
 
 app.get('/api/list', (req, res) => {
