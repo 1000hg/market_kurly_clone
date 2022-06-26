51b78f1fd77cebbdf997aeecedfa643ed9c0add7
yskim <alxzkf123@gmail.com>
2022-06-26 12:39:31 +0900

    build

--- packages/server/server.js
+++ packages/server/server.js
@@ -1,10 +1,11 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/', (req, res) => {
-    res.send('한글 되니?');
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
 })
 
 app.get('/api/list', (req, res) => {
