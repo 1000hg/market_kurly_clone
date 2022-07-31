800d9880a3e87e6bda68820ab88e45fc5df5541f
Signalzero <chlehdnjs@gmail.com>
2022-07-31 16:53:01 +0900

    first commit

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -3,47 +3,47 @@ import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+// test for commit
 function App() {
-    const [list, setList] = useState([]);
-    const [item, setItem] = useState({});
+  const [list, setList] = useState([]);
+  const [item, setItem] = useState({});
 
-    useEffect(() => {
-        (async () => {
-            const { data: resultList } = await axios.get('/api/list');
-            setList(resultList);
+  useEffect(() => {
+    (async () => {
+      const { data: resultList } = await axios.get('/api/list');
+      setList(resultList);
 
-            const { data: result } = await axios.get('/api/item');
-            setItem(result);
-        })();
-    }, []);
+      const { data: result } = await axios.get('/api/item');
+      setItem(result);
+    })();
+  }, []);
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
 
-                <div>List</div>
-                {list.map((item, idx) => (
-                    <div key={idx}>{`${item.id}/${item.name}`}</div>
-                ))}
+        <div>List</div>
+        {list.map((item, idx) => (
+          <div key={idx}>{`${item.id}/${item.name}`}</div>
+        ))}
 
-                <div>item</div>
-                <div>{`${item.id}/${item.name}`}</div>
-            </header>
-
-        </div>
-    );
+        <div>item</div>
+        <div>{`${item.id}/${item.name}`}</div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
