29ea9279281ba59888ea6ba73709ec04e1786367
jeong gh <gwanmu@gmail.com>
2022-08-10 00:00:46 +0900

    [edit]routes/users.js

--- packages/server/routes/users.js
+++ packages/server/routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const usersController = require("../controller/users.js");
+const isAuth = require('../common/auth.js');
 
 // 비밀번호 재설정
 router.put("/resetpw", usersController.resetPassword);
