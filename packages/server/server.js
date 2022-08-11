45a55c18bf227861ea8caa597cf359a0f64c99be
yskim <alxzkf123@gmail.com>
2022-08-12 00:44:33 +0900

    [edit] product_view

--- packages/server/server.js
+++ packages/server/server.js
@@ -11,26 +11,6 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-app.get('/api/list', (req, res) => {
-  res.json([
-    { id: 'test1', name: '테스트1' },
-    { id: 'test2', name: '테스트2' },
-    { id: 'test3', name: '테스트3' },
-    { id: 'test4', name: '테스트4' },
-  ]);
-});
-
-app.get('/api/item', (req, res) => {
-  res.json({
-    id: 'test1',
-    name: '테스트1',
-  });
-});
-
 app.use('/', indexRouter);
 
 app.listen(process.env.PORT || 8080, (err) => {
--- packages/server/services/product_view.js
+++ packages/server/services/product_view.js
@@ -58,9 +58,11 @@ async function createProductView(data) {
 
 async function findProductViewList() {
     try {
-      const result = await db.query(`SELECT * FROM tb_product_view as tb1 
-      LEFT JOIN tb_product as tb2
-      on tb1.product_seq = tb2.product_seq where tb1.product_view_status = 1`);
+      const result = await db.query(`SELECT tb1.*, tb2.*, GROUP_CONCAT(tb3.product_img) as product_img
+      FROM tb_product_view as tb1 
+            LEFT JOIN tb_product as tb2 on tb1.product_seq = tb2.product_seq
+            LEFT JOIN tb_product_img as tb3 on tb2.product_seq = tb3.product_seq
+            group by tb1.product_view_seq`);
       if(result) {
         serviceStatus.staus = 200
         serviceStatus.msg = '상품 조회에 성공하였습니다.'
