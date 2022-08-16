da118800fe734c6c8a147cb252de9e384cd0b6f9
Chomin21 <cmkbooty21@naver.com>
2022-08-16 21:31:43 +0900

    /api 추가

--- packages/server/server.js
+++ packages/server/server.js
@@ -1,22 +1,23 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const dotenv = require('dotenv');
-const indexRouter = require('./routes');
+const cors = require("cors");
+const dotenv = require("dotenv");
+const indexRouter = require("./routes");
 dotenv.config();
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
+app.use("/api", indexRouter);
 
 app.listen(process.env.PORT || 8080, (err) => {
   if (err) {
-    console.log('err 발생');
+    console.log("err 발생");
   }
 
-  console.log('정상구동');
+  console.log("정상구동");
 });
