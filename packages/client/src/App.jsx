d2ef075c36a2216c524dc51716cc20e44dfc7b83
Chomin21 <cmkbooty21@naver.com>
2022-08-21 16:58:34 +0900

    findId, findPwd, findIdConfirm 라우팅 추가

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,31 +1,40 @@
-import styles from './App.module.css';
-import MainPage from './routers/MainPage.jsx';
-import { Routes, Route } from 'react-router-dom';
+import styles from "./App.module.css";
+import MainPage from "./routers/MainPage.jsx";
+import { Routes, Route } from "react-router-dom";
 
-import LoginPage from './routers/LoginPage';
-import SignUpPage from './routers/SignupPage';
-import AddressPage from './routers/AddressPage';
-import AddressResult from './components/addressResult';
+import LoginPage from "./routers/LoginPage";
+import SignUpPage from "./routers/SignupPage";
+import AddressPage from "./routers/AddressPage";
+import AddressResult from "./components/addressResult";
+import FindIdPage from "./routers/FindIdPage";
+import FindPwdPage from "./routers/FindPwdPage";
+import FindIdConfirmPage from "./routers/FindIdConfirmPage";
 
 function App({ authService }) {
   return (
     <div className={styles.app}>
       <Routes>
-        <Route path='/' element={<MainPage />}></Route>
+        <Route path="/" element={<MainPage />}></Route>
         <Route
-          path='/login'
+          path="/login"
           element={<LoginPage authService={authService} />}
         ></Route>
+        <Route path="/login/findId" element={<FindIdPage />}></Route>
+        <Route path="/login/findPwd" element={<FindPwdPage />}></Route>
         <Route
-          path='/signup'
+          path="/login/findIdConfirm"
+          element={<FindIdConfirmPage />}
+        ></Route>
+        <Route
+          path="/signup"
           element={<SignUpPage authService={authService} />}
         ></Route>
         <Route
-          path='/address/shipping-address'
+          path="/address/shipping-address"
           element={<AddressPage />}
         ></Route>
         <Route
-          path='/address/shipping-address/result'
+          path="/address/shipping-address/result"
           element={<AddressResult />}
         ></Route>
       </Routes>
