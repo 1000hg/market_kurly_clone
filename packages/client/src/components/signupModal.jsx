0bf76e300e0fdcc5f7ac385bfd7b7d28ec2e2ff2
Signalzero <chlehdnjs@gmail.com>
2022-08-23 19:07:38 +0900

    add modal page

---
+++ packages/client/src/components/signupModal.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useState } from 'react';
+import styles from '../css/SignupModal.module.css';
+
+const SignupModal = ({ setModalOpen, title }) => {
+  const [notice, SetNotice] = useState();
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.popup}>
+        <div className={styles.content}>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.footer}>
+            <button className={styles.button} onClick={closeModal}>
+              확인
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignupModal;
