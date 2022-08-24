d6c01a6da55b89058a4d4c89ae45725d89813d1b
Chomin21 <cmkbooty21@naver.com>
2022-08-24 21:01:43 +0900

    아이디 찾기 페이지 수정
    - 아이디 찾기 페이지 퍼블리싱
    - 조건별 특정 태그 활성화/비활성화 처리 (추가 작업 필요)
    - 휴대폰 인증 연동
    - 이메일 인증 연동

--- packages/client/src/routers/FindIdPage.jsx
+++ packages/client/src/routers/FindIdPage.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useEffect, useRef } from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import MainFooter from "../components/mainFooter";
@@ -47,25 +48,88 @@ function TabContent({ tab }) {
   const navigate = useNavigate();
   const [rqFirst, setRqFirst] = useState(false);
   const [rqSecond, setRqSecond] = useState(false);
+  const [verify, setVerify] = useState(false);
+  const [verifyCd, setVerifyCd] = useState("");
+  const [rqVerify, setRqVerify] = useState(false);
+  const [verifyCdContent, serVerifyCdContent] =
+    useState("인증번호를 입력해주세요");
+  const verifyCdRef = useRef();
+  useEffect(() => {
+    setName("");
+    setPhone("");
+    setEmail("");
+    setVerifyCd("");
+    setRqFirst(false);
+    setRqSecond(false);
+    setVerify(false);
+    setRqVerify(false);
+  }, [tab]);
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
 
-    if (tab == 1) {
+    if (tab == 0) {
+      axios
+        .post("/api/verify/sms", { user_name: name, user_phone: phone })
+        .then((res) => {
+          console.log(res);
+          res.data.tab = tab;
+          setVerify(true);
+        })
+        .catch((e) => {
+          alert("다시 확인");
+        });
+    } else if (tab == 1) {
       axios
         .post("/api/verify/email", { user_name: name, user_email: email })
         .then((res) => {
           console.log(res);
-          return navigate("/login/findIdConfirm", { state: res.data });
+          res.data.email = email;
+          res.data.name = name;
+          res.data.tab = tab;
+          return navigate("/login/findIdConfirm", {
+            state: res.data,
+          });
         })
         .catch((e) => {
           alert("다시 확인");
         });
     }
   };
+  const onCdChangeHandler = (e) => {
+    setVerifyCd(e.currentTarget.value);
+
+    if (verifyCdRef.current.value == "") {
+      serVerifyCdContent("인증번호를 입력해주세요");
+      setRqVerify(false);
+    } else if (verifyCdRef.current.value.length < 7) {
+      serVerifyCdContent("7자리를 입력해주세요");
+    } else {
+      serVerifyCdContent("");
+      setRqVerify(true);
+    }
+  };
+
+  const sendIdVerify = () => {
+    axios
+      .post("/api/verify/sms/id", {
+        user_name: name,
+        user_phone: phone,
+        verify_code: verifyCd,
+      })
+      .then((res) => {
+        console.log(res);
+        return navigate("/login/findIdConfirm", {
+          state: res.data,
+        });
+      })
+      .catch((e) => {
+        alert("다시 확인");
+      });
+  };
 
   const onInit = (e) => {
-    if (name != "" && phone != "") {
+    if ((name != "" && phone != "") || (name != "" && email != "")) {
       return true;
     } else {
       return false;
@@ -73,6 +137,7 @@ function TabContent({ tab }) {
   };
   if (tab == 0) {
     //휴대폰 인증
+
     return (
       <div>
         <form className={styles.inputForm} onSubmit={onSubmitHandler}>
@@ -90,8 +155,13 @@ function TabContent({ tab }) {
                 type="text"
                 id="name"
                 placeholder="이름을 입력해주세요"
+                value={name}
               />
-              <button className={styles.delBtn}></button>
+              <button
+                type="button"
+                onClick={() => setName("")}
+                className={name == "" ? styles.delBtnNone : styles.delBtn}
+              ></button>
             </div>
             <p className={rqFirst ? `${styles.required}` : `${styles.hidden}`}>
               가입 시 등록한 이름을 입력해 주세요.
@@ -111,11 +181,121 @@ function TabContent({ tab }) {
                 type="tel"
                 id="phone"
                 placeholder="휴대폰 번호를 입력해주세요"
+                value={phone}
               />
-              <button className={styles.delBtn}></button>
+              <button
+                type="button"
+                onClick={() => setPhone("")}
+                className={phone == "" ? styles.hidden : styles.delBtn}
+              ></button>
+            </div>
+            {rqSecond && (
+              <p className={styles.required}>
+                가입 시 등록한 휴대폰 번호를 입력해 주세요.
+              </p>
+            )}
+          </div>
+
+          {verify && (
+            <div className={styles.divInput}>
+              <label className={styles.inputTitle} htmlFor="verifyCd">
+                인증번호
+              </label>
+              <div className={styles.relDiv}>
+                <input
+                  onChange={onCdChangeHandler}
+                  className={styles.inputContent}
+                  type="text"
+                  id="verifyCd"
+                  placeholder="인증번호 7자리"
+                  ref={verifyCdRef}
+                  autoFocus
+                />
+                <p className={styles.required}>{verifyCdContent}</p>
+              </div>
+            </div>
+          )}
+
+          {verify ? (
+            <button
+              onClick={sendIdVerify}
+              className={
+                rqVerify ? `${styles.submitBtn}` : `${styles.submitBtnDis}`
+              }
+              disabled={rqVerify ? false : true}
+            >
+              확인
+            </button>
+          ) : (
+            <button
+              className={
+                !rqFirst && !rqSecond && onInit()
+                  ? `${styles.submitBtn}`
+                  : `${styles.submitBtnDis}`
+              }
+              disabled={!rqFirst && !rqSecond && onInit() ? false : true}
+            >
+              인증번호 받기
+            </button>
+          )}
+        </form>
+      </div>
+    );
+  } else if (tab == 1) {
+    //이메일 인증
+    return (
+      <div>
+        <form className={styles.inputForm} onSubmit={onSubmitHandler}>
+          <div className={styles.divInput}>
+            <label className={styles.inputTitle} htmlFor="name">
+              이름
+            </label>
+            <div className={styles.relDiv}>
+              <input
+                onBlur={() =>
+                  name == "" ? setRqFirst(true) : setRqFirst(false)
+                }
+                onChange={(e) => setName(e.currentTarget.value)}
+                className={styles.inputContent}
+                type="text"
+                id="name"
+                placeholder="이름을 입력해주세요"
+                value={name}
+              />
+              <button
+                type="button"
+                onClick={() => setName("")}
+                className={name == "" ? styles.hidden : styles.delBtn}
+              ></button>
+            </div>
+            <p className={rqFirst ? `${styles.required}` : `${styles.hidden}`}>
+              가입 시 등록한 이름을 입력해 주세요.
+            </p>
+          </div>
+          <div className={styles.divInput}>
+            <label className={styles.inputTitle} htmlFor="phone">
+              이메일
+            </label>
+            <div className={styles.relDiv}>
+              <input
+                onBlur={() =>
+                  email == "" ? setRqSecond(true) : setRqSecond(false)
+                }
+                onChange={(e) => setEmail(e.currentTarget.value)}
+                className={styles.inputContent}
+                type="email"
+                id="email"
+                placeholder="이메일을 입력해주세요"
+                value={email}
+              />
+              <button
+                type="button"
+                onClick={() => setEmail("")}
+                className={email == "" ? styles.delBtnNone : styles.delBtn}
+              ></button>
             </div>
             <p className={rqSecond ? `${styles.required}` : `${styles.hidden}`}>
-              가입 시 등록한 휴대폰 번호를 입력해 주세요.
+              가입 시 등록한 이메일을 입력해 주세요.
             </p>
           </div>
 
@@ -127,31 +307,8 @@ function TabContent({ tab }) {
             }
             disabled={!rqFirst && !rqSecond && onInit() ? false : true}
           >
-            인증번호 받기
+            확인
           </button>
-        </form>
-      </div>
-    );
-  } else if (tab == 1) {
-    //이메일 인증
-    return (
-      <div>
-        <form onSubmit={onSubmitHandler}>
-          <label htmlFor="name">이름</label>
-          <input
-            onChange={(e) => setName(e.currentTarget.value)}
-            type="text"
-            id="name"
-            placeholder="이름을 입력해주세요"
-          />
-          <label htmlFor="email">이메일</label>
-          <input
-            onChange={(e) => setEmail(e.currentTarget.value)}
-            type="email"
-            id="email"
-            placeholder="이메일을 입력해주세요"
-          />
-          <button>확인</button>
         </form>
       </div>
     );
