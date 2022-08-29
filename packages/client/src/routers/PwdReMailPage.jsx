3d803b832b978ee67ed25b9815f5de1ccab7c032
Chomin21 <cmkbooty21@naver.com>
2022-08-30 00:42:09 +0900

    라우팅 추가 및 페이지 추가
    - 비밀번호 메일 발송 페이지, 비밀번호 재설성 페이지 추가 및 라우팅 추가

---
+++ packages/client/src/routers/PwdReMailPage.jsx
@@ -0,0 +1,12 @@
+import MainFooter from "../components/mainFooter";
+import MainHeader from "../components/mainHeader";
+
+export default function PwdRemailPage() {
+  return (
+    <div>
+      <MainHeader />
+      <div>비밀번호 재설정 메일 발송했는데요</div>
+      <MainFooter />
+    </div>
+  );
+}
