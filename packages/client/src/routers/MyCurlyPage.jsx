25cf01918d9c88c93b77b73fa70d302bd0360734
Signalzero <chlehdnjs@gmail.com>
2022-08-30 01:06:37 +0900

    fix ignore case to false

--- packages/client/src/routers/MyCurlyPage.jsx
+++ packages/client/src/routers/MyCurlyPage.jsx
@@ -1,4 +1,26 @@
-function MyCurlyPage() {
-  return <div>마이컬리인데요</div>;
-}
+import React from 'react';
+import MainHeader from '../components/mainHeader';
+import MainFooter from '../components/mainFooter';
+import styles from '../css/MyCurlyPage.module.css';
+
+const MyCurlyPage = () => {
+  return (
+    <>
+      <MainHeader />
+      <div className={styles.signup}>
+        <div className={styles.title}>회원가입</div>
+        <div className={styles.container}>
+          <div classs={styles.info_container}>
+            <div className={styles.notice}>
+              <span style={{ color: 'rgb(238, 106, 123)' }}>*</span>
+              필수입력사항
+            </div>
+          </div>
+        </div>
+      </div>
+      <MainFooter />
+    </>
+  );
+};
+
 export default MyCurlyPage;
