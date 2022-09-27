4351d76c89e179b2374fa7a72d3a87db29fe84e7
Chomin21 <cmkbooty21@naver.com>
2022-09-28 02:15:24 +0900

    검색 결과 페이지 구현
    - 검색 시 버튼 클릭으로 이동하는 경우 리렌더링 안되는 문제 수정.
    - query.keyword를 기준으로 data값 세팅 추가.
    - 결과 출력 시 상품 이름 띄우기.

--- packages/client/src/routers/SearchResultPage.jsx
+++ packages/client/src/routers/SearchResultPage.jsx
@@ -1,56 +1,55 @@
 import MainFooter from "../components/mainFooter";
 import MainHeader from "../components/mainHeader";
 import CardProduct from "../components/cardProduct";
+import styles from "../css/SearchResultPage.module.css";
 
 import qs from "query-string";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import axios from "axios";
 
 export default function SearchResultPage() {
-  const [productName, setProductName] = useState("");
   const location = useLocation();
-  const navigate = useNavigate();
-  const data = location.state;
+  const [data, setData] = useState(location.state);
   const searchParams = location.search;
   const query = qs.parse(searchParams);
-
-  console.log("searchParams : ", searchParams);
-  console.log("query : ", query);
-  console.log("query.keyword : ", query.keyword);
-  console.log("data : ", data);
 
   //url경로에 직접 입력한 경우
   useEffect(() => {
     if (data === null || data === "") {
-      console.log(query);
       axios
         .get("/api/product/view/search", {
           params: { product_name: query.keyword },
         })
         .then((res) => {
-          console.log("주소에 직접 입력 ::: ", res);
-          navigate("/product/search?keyword=" + query.keyword, {
-            state: res.data,
-          });
+          setData(res.data);
         })
         .catch((e) => {
-          console.log(e);
+          // console.log(e);
         });
     }
-  }, []);
+    /*버튼 클릭으로 검색한 경우
+    같은 페이지로 navigate시 리렌더링이 안돼서 keyword변경 시 렌더링 추가.
+    */
+    setData(location.state);
+  }, [query.keyword]);
 
   return (
     <>
       <MainHeader />
-      {/* <section>
-        <div>
-          {data.map((item, idx) => {
-            return <CardProduct key={idx} item={item} />;
-          })}
+      <div className={styles.resultD}>
+        '<span className={styles.resultS}>{query.keyword}</span>'에 대한
+        검색결과
+      </div>
+      <section className={styles.searchProductSec}>
+        <div className={styles.searchProductView}>
+          {data !== null &&
+            data.responseData.map((item, idx) => {
+              return <CardProduct key={idx} item={item} opt={0} />;
+            })}
         </div>
-      </section> */}
+      </section>
       <MainFooter />
     </>
   );
